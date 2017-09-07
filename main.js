e846c1e62b4e7d43c6ccacf9228df0e3f3fef4b1
anmitsux <abcdabcdec1@gmail.com>
2017-09-08 01:50:14 +0900

    update

--- main.js
+++ main.js
@@ -1,43 +1,75 @@
-
-Vue.component('cards',{
-  template:'<div class="box"><div class="card"><div v-for="card in cards"><div class="button_area_wrap" :class="{ \'is-active\':card.isActive}"><div class="button_area"><a class="button is-outlined is-black is-large" @click="skipCard(card)">Skip</a><a class="button is-danger is-large is-outlined">Thanks!</a></div></div></div><slot></slot><div class="section"></div></div></div>',
-  data(){
-    return {cards:[]};
-  },
-  created(){
-    this.cards=this.$children;
-  },
-  methods:{
-    skipCard(skipCard){
-        this.cards.forEach(card=>{
-          card.isActive= (card.name == skipCard.name);
-        });
-    }
-  }
-});
-
-Vue.component('card',{
-  template:'<div v-show="isActive"><div class="card-image"><figure class="image is-4by3"><img :src="url" alt="Image"></figure></div><div class="card-content"><div class="media"><div class="media-content"><p class="title is-4">{{name}} {{old}}olds {{city}}</p><p class="subtitle is-6"><i class="fa fa-commenting-o" aria-hidden="true"></i> Im so free!</p></div></div><hr><p class="title is-4">Introduction</p><div class="content">{{content}}</div><hr><slot></slot></div></div>',
-  data(){
-    return{
-      isActive:false
-    };
-  },
-  props:{
-    name:{required:true},
-    city:{required:true},
-    old:{required:true},
-    url:{required:true},
-    content:{required:true},
-    selected:{default:false}
-  },
-  mounted(){
-    this.isActive=this.selected;
-  },
-});
 Vue.component('nav_area',{
   template:'<div class="hero is-primary"><div class="hero-body"><div class="container"><h1 class="title">Pairs_Spa</h1></div></div></div>',
 });
+Vue.component('modal',{
+  template:'<div class="modal is-active"><div class="modal-background"></div><div class="modal-content"><slot></slot></div><button class="modal-close is-large" aria-label="close" @click="$emit(\'close\')"></button></div>',
+});
+
 new Vue({
   el:'#app',
+  data:{
+    cards:[{
+      name:"ketty",
+      city:"tokyo",
+      old:"31",
+      url:"assets/profile_img1.jpg",
+      content:"1Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus nec iaculis mauris.",
+      isActive:true
+    },
+    {
+      name:"micky",
+      city:"tokyo",
+      old:"28",
+      url:"assets/profile_img2.jpg",
+      content:"2Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus nec iaculis mauris.",
+      isActive:false
+    },
+    {
+      name:"chaly",
+      city:"tokyo",
+      old:"48",
+      url:"assets/profile_img3.jpg",
+      content:"3Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus nec iaculis mauris.",
+      isActive:false
+    }],
+    modalVisible:false,
+    lastLiVisible:false
+  },
+  methods: {
+    skipCard(card) {
+      // card.isActive=false;
+      console.log(card.isActive);
+      // this.cards.splice(this.cards.indexOf(card), 1);
+
+      if(this.cards.indexOf(card)==2){
+        this.cards[this.cards.indexOf(card)].isActive=false;
+        this.lastLiVisible=true;
+      }else{
+        this.cards[this.cards.indexOf(card)].isActive=false;
+        this.cards[this.cards.indexOf(card)+1].isActive=true;
+      }
+      console.log(card.name);
+    },
+    thanks(card){
+      console.log(card.isActive);
+      // this.cards.splice(this.cards.indexOf(card), 1);
+      if(this.cards.indexOf(card)==2){
+        this.cards[this.cards.indexOf(card)].isActive=false;
+        this.lastLiVisible=true;
+        this.modalVisible=false;
+      }else{
+        this.cards[this.cards.indexOf(card)].isActive=false;
+        this.cards[this.cards.indexOf(card)+1].isActive=true;
+        this.modalVisible=false;
+      }
+
+      // card.isActive=false;
+      console.log(card.name);
+    }
+  },
+  computed:{
+    zindexV:function(index){
+      return this.cards.length-1-index;
+    }
+  }
 });
